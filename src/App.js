6d924f481782e7df9b78586fae704b3ef2be3423
08fdange <08fdange@gmail.com>
2021-02-18 21:09:01 -0500

    First commit, simple calc app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const [result, setResult] = useState(0);
+  const [firstNum, setFirstNum] = useState(0);
+  const [secNum, setSecNum] = useState(0);
+  const [operator, setOperator] = useState('+');
+
+  const handleFirstNumChange = (event) => {
+    setFirstNum(event.target.value)
+  }
+
+  const handleSecNumChange = (event) => {
+    setSecNum(event.target.value)
+  }
+
+  const handleOpChange = (event) => {
+    setOperator(event.target.value)
+  }
+
+  const handleCalc = () => {
+    setResult(eval(firstNum + operator + secNum));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Calculator App</h1>
+      <br/>
+      <h1>{result}</h1>
+      <br/>
+      <span>
+        <input type='number' name='firstNumber' onChange={handleFirstNumChange} value={firstNum}></input>
+        <select name='operation' onChange={handleOpChange}>
+          <option value='+'>+</option>
+          <option value='-'>-</option>
+          <option value='*'>*</option>
+          <option value='/'>/</option>
+        </select>
+        <input type='number' name='secondNumber' onChange={handleSecNumChange} value={secNum}></input>
+      </span>
+      <button
+        onClick={handleCalc}
+      >Calculate
+      </button>
     </div>
   );
 }
